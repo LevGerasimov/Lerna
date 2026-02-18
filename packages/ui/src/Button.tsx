dbc72f242f98f3aa257f5aaedd81732a0424068b
naetdragon1-crypto <naet.dragon.1@gmail.com>
2026-02-18 14:18:44 +0100

    start

---
+++ packages/ui/src/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type ButtonProps = {
+  children: React.ReactNode;
+};
+
+export const Button = ({ children }: ButtonProps) => {
+  return (
+    <button style={{ padding: "8px 16px", background: "black", color: "white" }}>
+      {children}
+    </button>
+  );
+};
